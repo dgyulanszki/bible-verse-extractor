959be3ce311afc5bbacbcb60eff3b09dbf395ea9
AzureAD\DavidGyulanszki <david_gyulanszki@epam.com>
2026-05-27 21:15:46 +0200

    Spring Boot project init.

---
+++ src/main/java/hu/szegedibibliaszol/bible_verse_extractor/BibleVerseExtractorApplication.java
@@ -0,0 +1,13 @@
+package hu.szegedibibliaszol.bible_verse_extractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleVerseExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibleVerseExtractorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/szegedibibliaszol/bible_verse_extractor/BibleVerseExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.szegedibibliaszol.bible_verse_extractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibleVerseExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
